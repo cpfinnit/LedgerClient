e0572cb04883f6430a546b1e3621878ca5541ab0
cpfinnit <jhunter46@gmail.com>
2013-12-08 17:05:12 -0700

    End Wk7

--- src/ledgerclient/LedgerRestfulClient.java
+++ src/ledgerclient/LedgerRestfulClient.java
@@ -33,65 +33,56 @@ public class LedgerRestfulClient {
         
         Client client = ClientBuilder.newBuilder().build();
         
-        ////*************************XML Media Type*************************////
-        //Create user XML;
+        //Crate User XML
         WebTarget userTarget = client.target(getBaseURI());
         Form form = new Form().param("username", "Bruce Wayne");
-        Response newResponse = userTarget.path("users").request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).post(Entity.form(form));
+        Response newResponse = userTarget.path("users").queryParam("reqFormat", "xml").request().post(Entity.form(form));
         User user = newResponse.readEntity(User.class);
         System.out.println("New User: " + user.getUsername());
-        
-        //Get User XML
-        WebTarget readUserTarget = client.target(getBaseURI());
-        Response readResponse = readUserTarget.path("users").path("Bruce Wayne").request(MediaType.APPLICATION_XML).get();
-        User newUser = readResponse.readEntity(User.class);
-        System.out.println("Get User XML Username: " + newUser.getUsername());
-        
-        //Update User XML
-        WebTarget updateUserTarget = client.target(getBaseURI());
-        Form updateForm = new Form().param("_username", "Batman");
-        Response updateResponse = updateUserTarget.path("users").path("Bruce Wayne").request(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.APPLICATION_XML).put(Entity.form(updateForm));
-        User updatedXMLUser = updateResponse.readEntity(User.class);
-        System.out.println("Updated XML Username: " + updatedXMLUser.getUsername());
-        
-        //Delete User XML
-        WebTarget deleteUser = client.target(getBaseURI());
-        Response deleteResponse = deleteUser.path("users").path("Batman").request().delete();
-        deleteResponse.readEntity(String.class);
-        
-        
-////*************************Plain  Text Media Type*************************////
+
         //Create user String
         WebTarget newUserTarget = client.target(getBaseURI());
         Form newForm = new Form().param("username", "Clark Kent");
-        Response newUserResponse = newUserTarget.path("users").request(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.TEXT_PLAIN).post(Entity.form(newForm));
+        Response newUserResponse = newUserTarget.path("users").queryParam("reqFormat", "plain/text").request().post(Entity.form(newForm));
         System.out.println("New User Plain Text: " + newUserResponse.readEntity(String.class));
+        
+        //Get User XML
+        WebTarget readUserTarget = client.target(getBaseURI());
+        Response readResponse = readUserTarget.path("users").path("1").queryParam("reqFormat", "xml").request().get();
+        User newUser = readResponse.readEntity(User.class);
+        System.out.println("Get User XML Username: " + newUser.getUsername());
         
         //Get User String
         WebTarget readUserTargetString = client.target(getBaseURI());
-        Response readResponseString = readUserTargetString.path("users").path("Clark Kent").request(MediaType.TEXT_PLAIN).accept(MediaType.TEXT_PLAIN).get();
+        Response readResponseString = readUserTargetString.path("users").path("1").queryParam("reqFormat", "plain/text").request().get();
         System.out.println("Get Plain Text Username: " + readResponseString.readEntity(String.class));
+       
+        //Update User XML
+        WebTarget updateUserTarget = client.target(getBaseURI());
+        Form updateForm = new Form().param("_username", "Batman");
+        Response updateResponse = updateUserTarget.path("users").path("Bruce Wayne").queryParam("reqFormat", "xml").request().put(Entity.form(updateForm));
+        User updatedXMLUser = updateResponse.readEntity(User.class);
+        System.out.println("Updated XML Username: " + updatedXMLUser.getUsername());
         
         //Update User String
         WebTarget updateUserTargetString = client.target(getBaseURI());
-        Form updateFormString = new Form().param("_username", "Superman");
-        Response updateResponseString = updateUserTargetString.path("users").path("Clark Kent").request(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.TEXT_PLAIN).put(Entity.form(updateFormString));
+        Form updateForm1 = new Form().param("_username", "Superman");
+        Response updateResponseString = updateUserTargetString.path("users").path("Clark Kent").queryParam("reqFormat", "plain/text").request().put(Entity.form(updateForm1));
         System.out.println("Updated Plain Text Username: " + updateResponseString.readEntity(String.class));
-
+        
         //Delete User String
         WebTarget deleteUserString = client.target(getBaseURI());
-        Response deleteResponseString = deleteUserString.path("users").path("Superman").request(MediaType.TEXT_PLAIN).accept(MediaType.TEXT_PLAIN).delete();
+        Response deleteResponseString = deleteUserString.path("users").path("Batman").queryParam("reqFormat", "plain/text").request().delete();
         deleteResponseString.readEntity(String.class);
+ 
+        //Delete User XML
+        WebTarget deleteUser = client.target(getBaseURI());
+        Response deleteResponse = deleteUser.path("users").path("Superman").queryParam("reqFormat", "xml").request().delete();
+        deleteResponse.readEntity(String.class);
         
-
-        //This should invoke my custome exception and come back with a 404, but my UserNotFoundMapper is not being invoked. 
-        WebTarget readUserTargetFail = client.target(getBaseURI());
-        Response readResponseFail = readUserTargetFail.path("users").path("No One").request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).get();
-        System.out.println(readResponseFail.readEntity(String.class));
-        
-       
     }
     
+       
     
     private static URI getBaseURI() {
         return UriBuilder.fromUri("http://localhost:8080/LedgerApp/rest").build();
